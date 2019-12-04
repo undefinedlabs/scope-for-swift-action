589149b787121700f1325b0f7e7ca8931627bc65
Ignacio Bonafonte <nacho@undefinedlabs.com>
2019-12-04 09:00:31 +0100

    First functional change

--- dist/index.js
+++ dist/index.js
@@ -5,58 +5,75 @@ const fs = require('fs')
 const shell = require('shelljs');
 
 const SCOPE_DSN = 'SCOPE_DSN';
+const scopeDir = '.scope_dir';
+const derivedDataPath = scopeDir + '/derived'
+const xctestDir =  derivedDataPath + '/Build/Products/'
+const testrunJson = scopeDir + '/testrun.json'
+var dsn = core.getInput('dsn') || process.env[SCOPE_DSN];
 
 async function run() {
     try {
 
-      var dsn = core.getInput('dsn') || process.env[SCOPE_DSN];
-        dsn = 'Hola'
+
       if (!dsn) {
         throw Error('Cannot find the Scope DSN');
       }
 
-      var sdk = core.getInput('sdk') || 'iphonesimulator';
-      var destination = core.getInput('destination') || 'platform=iOS Simulator,name=iPhone 11';
+      let sdk = core.getInput('sdk') || 'iphonesimulator';
+      let destination = core.getInput('destination') || 'platform=iOS Simulator,name=iPhone 11';
 
       //Read project
       const workspace  = getWorkspace();
       const xcodeproj = getXCodeProj();
 
-      var projectParameter
+      var projectParameter;
 
       if(workspace) {
-          console.log(`Workspace: ${workspace}`);
-          projectParameter = '-workspace ' + workspace
+          console.log(`Workspace selected: ${workspace}`);
+          projectParameter = '-workspace ' + workspace;
       } else if (xcodeproj) {
-          console.log(`Project: ${xcodeproj}`);
-          projectParameter = '-project ' + xcodeproj
+          console.log(`Project selected: ${xcodeproj}`);
+          projectParameter = '-project ' + xcodeproj;
       } else {
           throw Error('Unable to find the workspace or xcodeproj. Please set with.workspace or.xcodeproj');
       }
 
       const scheme = getScheme(workspace, xcodeproj);
-      console.log(`Scheme: ${scheme}`);
+      console.log(`Scheme selected: ${scheme}`);
 
       //copy configfile
-        let configfileName = 'scopeConfig.xcconfig'
-        let scopeDir = '.scope_dir'
-        let configFilePath = scopeDir + '/' + configfileName
+        let configfileName = 'scopeConfig.xcconfig';
+
+        let configFilePath = scopeDir + '/' + configfileName;
 
         if (!fs.existsSync(scopeDir)){
             fs.mkdirSync(scopeDir);
         }
-        fs.copyFileSync('dist/'+ configfileName, configFilePath)
+        fs.copyFileSync('dist/'+ configfileName, configFilePath);
 
         //build for testing
         let buildCommand = 'xcodebuild build-for-testing -xcconfig ' + configFilePath + ' ' + projectParameter +
-            ' -scheme ' + scheme + ' -sdk ' + sdk + ' -destination \'' + destination + '\' -derivedDataPath .scope_dir/derived'
+            ' -scheme ' + scheme + ' -sdk ' + sdk + ' -destination \"' + destination + '\" -derivedDataPath ' + derivedDataPath;
+        await exec.exec(buildCommand, null, { ignoreReturnCode: true });
 
 
-        let output = shell.exec(buildCommand, {silent: false})
+        //modify xctestrun with Scope variables
+        let testRun = getXCTestRun();
+        let plutilExportCommand = 'plutil -convert json -o ' + testrunJson + ' ' + testRun;
+        await exec.exec(plutilExportCommand, null, { ignoreReturnCode: true });
 
+        let jsonString = fs.readFileSync(testrunJson, "utf8");
+        const testTargets = JSON.parse(jsonString);
 
-        console.log(output);
+        Object.keys(testTargets).forEach(function (name,index,array) {
+            if( name.charAt(0) != '_' ) {
+                insertEnvVariables(testRun, name)
+            }
+        });
 
+        //run tests
+        let testCommand = 'xcodebuild test-without-building -xctestrun ' + testRun + ' -destination \"' + destination + '\"';
+        await exec.exec(testCommand, null, { ignoreReturnCode: true });
     } catch (error) {
       core.setFailed(error.message);
     }
@@ -64,37 +81,58 @@ async function run() {
 
 
 function getWorkspace() {
-    var workspace = core.getInput('workspace')
+    let workspace = core.getInput('workspace');
     if (!workspace) {
-        workspace = shell.ls().filter(function(file) { return file.match(/\.xcworkspace$/); })[0]
+        workspace = shell.ls().filter(function(file) { return file.match(/\.xcworkspace$/); })[0];
     }
     return workspace
 }
 
 function getXCodeProj() {
-    var xcodeproj = core.getInput('xcodeproj')
+    let xcodeproj = core.getInput('xcodeproj');
     if (!xcodeproj) {
-        xcodeproj = shell.ls().filter(function(file) { return file.match(/\.xcodeproj/); })[0]
+        xcodeproj = shell.ls().filter(function(file) { return file.match(/\.xcodeproj/); })[0];
     }
-    return xcodeproj
+    return xcodeproj;
 }
 
 function getScheme(workspace, xcodeproj) {
-    var scheme = core.getInput('scheme')
+    let scheme = core.getInput('scheme');
     if (!scheme) {
-        var command
+        let command;
         if(workspace) {
-            command = 'xcodebuild -workspace ' + workspace + ' -list -json'
+            command = 'xcodebuild -workspace ' + workspace + ' -list -json';
         } else {
-            command = 'xcodebuild -project ' + xcodeproj + ' -list -json'
+            command = 'xcodebuild -project ' + xcodeproj + ' -list -json';
         }
-        const info = JSON.parse(shell.exec(command, {silent: true}).stdout)
-        const aux = info.workspace || info.project
-        const schemes = aux.schemes
-        console.log('find schemes: ' + JSON.stringify(schemes))
-        scheme = intelligentSelectScheme(schemes, aux)
+        const info = JSON.parse(shell.exec(command, {silent: true}).stdout);
+        const aux = info.workspace || info.project;
+        const schemes = aux.schemes;
+        console.log('Available schemes: ' + JSON.stringify(schemes));
+        scheme = intelligentSelectScheme(schemes, aux);
     }
     return scheme
+}
+
+function getXCTestRun() {
+    const testrun = shell.ls(xctestDir).filter(function(file) { return file.match(/\.xctestrun$/); })[0];
+    return xctestDir + testrun
+}
+
+function insertEnvVariables( file, target) {
+    //insertEnvVariable('SCOPE_DSN', '\'$(SCOPE_DSN)\'', file, target );
+    insertEnvVariable('SCOPE_DSN', '\"' + dsn + '\"', file, target );
+    insertEnvVariable('SCOPE_COMMIT_SHA','\"$(SCOPE_COMMIT_SHA)\"', file, target );
+    insertEnvVariable('SCOPE_SOURCE_ROOT','\"$(SCOPE_SOURCE_ROOT)\"', file, target );
+    insertEnvVariable('GITHUB_REPOSITORY','\"$(GITHUB_REPOSITORY)\"', file, target );
+    insertEnvVariable('SCOPE_COMMIT_SHA','\"$(SCOPE_COMMIT_SHA)\"', file, target );
+    insertEnvVariable('SCOPE_INSTRUMENTATION_HTTP_PAYLOADS', "YES", file, target );
+    insertEnvVariable('SCOPE_SET_GLOBAL_TRACER', "YES", file, target );
+}
+
+function insertEnvVariable( name, value, file, target) {
+    let insertCommand = 'plutil -replace \"' + target + '.TestingEnvironmentVariables.' + name + '\" -string ' + value + ' ' + file;
+    exec.exec(insertCommand, null, { ignoreReturnCode: true });
 }
 
 
@@ -102,11 +140,11 @@ function intelligentSelectScheme(schemes, workspacePath) {
     if (schemes.length < 1) {
         return null
     }
-    const workspaceName = workspacePath.name
+    const workspaceName = workspacePath.name;
     if (schemes.includes(workspaceName)) {
         return workspaceName
     }
     return schemes[0]
 }
 
-run()
+run();
