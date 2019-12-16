42c43d1c0ee58ddd16731884d815ee7bc9affa74
Ignacio Bonafonte <nacho@undefinedlabs.com>
2019-12-16 17:32:41 +0100

    Don't run Code Path in debug mode

--- index.js
+++ index.js
@@ -281,7 +281,6 @@ async function runScopeCoverageWithSettings(buildSettings, dsn) {
             ...buildSettings,
             SCOPE_DSN: dsn,
             TMPDIR: os.tmpdir() + '/',
-            SCOPE_COVERAGE_DEBUG: 'true',
         },
         ignoreReturnCode: true
     })
