b5414e0be92365bfe7dd72d5979c6f87a3b5f32c
Ignacio Bonafonte <nacho@undefinedlabs.com>
2019-12-26 11:59:14 +0100

    Try deleting the coverage lines directly

--- dist/index.js
+++ dist/index.js
@@ -403,16 +403,7 @@ async function configureTestPlansForCoverage( projectParameter, scheme ) {
     //If uses testplan configure to use code coverage
     let file_list = recFindByExt('.','xctestplan');
     for(let testPlanFile of file_list ){
-        let rawdata = fs.readFileSync(testPlanFile);
-        console.log(`testPlanFile previous: ${rawdata}`);
-        let testPlan = JSON.parse(rawdata);
-        if( testPlan.defaultOptions.hasOwnProperty('codeCoverage') ){
-            delete testPlan.defaultOptions.codeCoverage
-        }
-        await io.rmRF(testPlanFile);
-        fs.writeFileSync(testPlanFile, JSON.stringify(testPlan));
-        let rawdata2 = fs.readFileSync(testPlanFile);
-        console.log(`testPlanFile after: ${rawdata2}`);
+        await deleteLinesContaining(testPlanFile, 'codeCoverage')
     }
 }
 
@@ -440,6 +431,34 @@ function recFindByExt(base,ext,files,result)
     return result
 }
 
+async function deleteLinesContaining( file, match ) {
+    fs.readFile(file, {encoding: 'utf-8'}, function(err, data) {
+        if (err) throw error;
+
+        let dataArray = data.split('\n'); // convert file data in an array
+        const searchKeyword = match; // we are looking for a line, contains, key word 'user1' in the file
+        let lastIndex = -1; // let say, we have not found the keyword
+
+        for (let index=0; index<dataArray.length; index++) {
+            if (dataArray[index].includes(searchKeyword)) { // check if a line contains the 'user1' keyword
+                lastIndex = index; // found a line includes a 'user1' keyword
+                break;
+            }
+        }
+
+        dataArray.splice(lastIndex, 1); // remove the keyword 'user1' from the data Array
+
+        // UPDATE FILE WITH NEW DATA
+        // IN CASE YOU WANT TO UPDATE THE CONTENT IN YOUR FILE
+        // THIS WILL REMOVE THE LINE CONTAINS 'user1' IN YOUR shuffle.txt FILE
+        const updatedData = dataArray.join('\n');
+        fs.writeFile(file, updatedData, (err) => {
+            if (err) throw err;
+            console.log ('Successfully updated the file data');
+        });
+
+    });
+}
 run();
 
 
